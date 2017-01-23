5cda0484729cbba678b7526533bb442005e642f9
venkat <bhvreddy88@gmail.com>
2017-01-23 08:13:19 +0530

    render html file eg.
    https://youtu.be/p5eCYKiZN-4?list=PL55RiY5tL51oGJorjEgl6NVeDbx_fO5jR

--- call html file/app.js
+++ call html file/app.js
@@ -1,2 +1,16 @@
-var html = require('html');
+var http = require('http');
 var fs = require('fs');
+
+function io(request,response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./calll.html',null, function(error,data){
+        if(error){
+            response.write('file not found');
+        }else{
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+http.createServer(io).listen(8080);
